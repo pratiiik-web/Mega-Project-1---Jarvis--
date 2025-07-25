12fe219ae1212fb0355b7d9927cefa22b11a0ea9
Pratik <karalepratik49@gmail.com>
2025-07-25 18:42:09 +0530

    initial commit

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "let it happen": "https://www.youtube.com/watch?v=pFptt7Cargc&list=RDpFptt7Cargc&start_radio=1&pp=ygUNbGV0IGl0IGhhcHBlbqAHAQ%3D%3D",
+    "la la land": "https://youtu.be/oTN7xO6emU0?list=RDoTN7xO6emU0",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
+}
