323980f7bb16bf599d61693f77408953a84caa3f
Pratik <karalepratik49@gmail.com>
2025-08-02 21:59:35 +0530

    Update newsapi key in config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 
 # Add your keys here
 openrouter_api_key = "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"
-newsapi = "<YOUR_NEWS_API_KEY>"
+newsapi = "fd9a96eb085e4c05a98e6a213cb990b2"
+
